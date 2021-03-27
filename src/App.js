b51d38deaaedda915e48ffaae9665344e7a2c783
Jameel Jiwani <jameeljiwani@gmail.com>
2021-03-26 23:01:01 -0400

    created initial landing page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import logo from './assets/icons/logo.png';
+import './App.scss';
+import { Layout, Menu, Row, Col } from 'antd';
+import LandingPage from './pages/LandingPage.js'
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Footer } = Layout;
 
 function App() {
   return (
     <>
       <Layout>
         <Header>
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
+          <Row>
+            <Col span={12}>
+              <img src={logo} className="logo" alt="logo" />
+              <h3>Website</h3>
+            </Col>
+            <Col span={12}>
+              <Menu theme="light" mode="horizontal" defaultSelectedKeys={['2']}>
+                <Menu.Item key="1">nav 1</Menu.Item>
+                <Menu.Item key="2">nav 2</Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item>
+              </Menu>
+            </Col>
+          </Row>
         </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <div className="site-layout-content">Content</div>
-        </Content>
+        <LandingPage/>
         <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
       </Layout>
     </>
